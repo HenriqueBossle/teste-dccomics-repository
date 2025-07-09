e58679da34985418c756fd9774270aa07ee3b493
Henrique Bossle <henriquegoncalvesbossle@gmail.com>
2025-07-09 17:38:52 -0300

    charada

---
+++ charadas.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="images/charadabatman.jpeg" alt="charada do batman" class="w-full h-auto">
+<?php
+
+echo "<h1>Olá morcego, aqui é o seu fim!!!</h1>";
+
+$num1 = 6;
+$num2 = 6;
+$num3 = 6;
+
+$meiameiameia = $num1 . $num2 . $num3;
+
+echo "$meiameiameia";
+
+
+
+?>
+
+</body>
+</html>
